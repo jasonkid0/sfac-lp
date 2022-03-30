234892acd8cf11cde34744bcfe40242734189282
Jason Casas <jason.carolino.casas@gmail.com>
2022-03-30 17:17:06 +0800

    first commit033022

---
+++ includes/conn.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "enrollmentlp";
+
+// $servername = "localhost";
+// $username = "u733437513_josh";
+// $password = "9JM!gh]9K";
+// $dbname = "u733437513_enrollmentlp";
+
+$db = new mysqli($servername, $username, $password, $dbname) or die($db->error);
